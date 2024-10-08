22d99951f6060494ee1ca7baeb4b03f44e81fa2e
Shangxian Xia <1032883@student.sae.edu.au>
2024-10-08 15:25:52 +1100

    - added some starting scripts

---
+++ Assets/gpg214 Project modifications/StreamingAudio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StreamingAudio : MonoBehaviour
+{
+
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
